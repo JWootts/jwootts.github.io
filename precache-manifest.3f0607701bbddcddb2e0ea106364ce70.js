4819dd380bb01b1dc5fa7eed9431b8f5359bb441
JWootts <jgall1300@gmail.com>
2023-02-24 00:45:11 -0500

    Updates

---
+++ precache-manifest.3f0607701bbddcddb2e0ea106364ce70.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "543a83fef8e97d69c064",
+    "url": "/static/css/main.ab279735.chunk.css"
+  },
+  {
+    "revision": "543a83fef8e97d69c064",
+    "url": "/static/js/main.543a83fe.chunk.js"
+  },
+  {
+    "revision": "ccdfce8b31b041c852ac",
+    "url": "/static/js/1.ccdfce8b.chunk.js"
+  },
+  {
+    "revision": "f4445f04b95a1d9b9eb2",
+    "url": "/static/css/2.e1bedd5f.chunk.css"
+  },
+  {
+    "revision": "f4445f04b95a1d9b9eb2",
+    "url": "/static/js/2.f4445f04.chunk.js"
+  },
+  {
+    "revision": "ae4ca73a90cddd0f1955",
+    "url": "/static/js/runtime~main.ae4ca73a.js"
+  },
+  {
+    "revision": "1a102014e14443ec0084a7d1877bc46e",
+    "url": "/static/media/logo.1a102014.png"
+  },
+  {
+    "revision": "de562854100b7bb2a3fe8d9f97954be1",
+    "url": "/static/media/canada-revenue-agency.de562854.png"
+  },
+  {
+    "revision": "317d27b66dd1f25291cf1d746619356b",
+    "url": "/static/media/Cineplex_logo.317d27b6.png"
+  },
+  {
+    "revision": "e1470dc6ddd3b99c541df02f09decb44",
+    "url": "/static/media/Fanshawe-logo.e1470dc6.png"
+  },
+  {
+    "revision": "0802b3463be80ae1fdd89768e7ac4d3e",
+    "url": "/static/media/ucc-logo.0802b346.png"
+  },
+  {
+    "revision": "dc678d449d56fd0c1c8a0e06490819dc",
+    "url": "/static/media/pfp.dc678d44.jpg"
+  },
+  {
+    "revision": "c0ff11dbbf5de927884d614198210875",
+    "url": "/static/media/JW_Resume.c0ff11db.pdf"
+  },
+  {
+    "revision": "6338678d88e591832f728c95f2c512ab",
+    "url": "/index.html"
+  }
+];
